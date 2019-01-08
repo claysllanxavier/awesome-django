142d7e0cbf1d34a4dd4d6fa580a02f336e51836d
Claysllan Xavier <claysllan-xavier@hotmail.com>
2019-01-07 22:14:22 -0300

    :fire: Utiliza biblioteca externa para gerar os forms de create e update
    - Exclui static files do projeto passado
    - Remove função e arquivos do pipenv
    - Cria o requirements com as dependências

--- awesome_django/settings.py
+++ awesome_django/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Apps do Projeto
     'core',
     'demo',
+
+    #Apps Externas
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
