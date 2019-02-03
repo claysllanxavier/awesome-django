9571dbf1d3b11dcb1b12564d4641d6c79c1e101e
Claysllan Xavier <claysllan-xavier@hotmail.com>
2019-02-03 16:00:16 -0300

    :rocket: Começa a utiliza o .env
    - Instala a dependência
    - Coloca o .env para funcionar no settings

--- awesome_django/settings.py
+++ awesome_django/settings.py
@@ -11,21 +11,27 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import environ
 
 from django.conf.global_settings import DATETIME_FORMAT
+
+env = environ.Env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOCAL = os.path.dirname(os.path.abspath(__file__))
 
+# reading .env file
+environ.Env.read_env(env.str(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2p@x*5cz5#7kpy_$@y+mdslhnk5kfd&n4^n&v3l-mkyiq^c!i2'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
