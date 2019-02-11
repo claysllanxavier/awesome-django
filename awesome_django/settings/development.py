0c4bf4559959d1d11f056aff2be8f2a0c7fca9bf
Claysllan Xavier <claysllan-xavier@hotmail.com>
2019-02-11 11:37:58 -0300

    :sparkles: Instala biblioteca de debug
    - Instala biblioteca somente no desenvolvimento
    - Coloca as urls da biblioteca
    - Coloca ela somente nas configurações de desenvolvimento

--- awesome_django/settings/development.py
+++ awesome_django/settings/development.py
@@ -3,7 +3,13 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
 ALLOWED_HOSTS = ['*']
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
@@ -17,3 +23,23 @@ DATABASES = {
 
 #Configurações de Email
 EMAIL_PORT = 1025
+
+DEBUG_TOOLBAR_PANELS = [
+       'debug_toolbar.panels.versions.VersionsPanel',
+       'debug_toolbar.panels.timer.TimerPanel',
+       'debug_toolbar.panels.settings.SettingsPanel',
+       'debug_toolbar.panels.headers.HeadersPanel',
+       'debug_toolbar.panels.request.RequestPanel',
+       'debug_toolbar.panels.sql.SQLPanel',
+       'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+       'debug_toolbar.panels.templates.TemplatesPanel',
+       'debug_toolbar.panels.cache.CachePanel',
+       'debug_toolbar.panels.signals.SignalsPanel',
+       'debug_toolbar.panels.logging.LoggingPanel',
+       'debug_toolbar.panels.redirects.RedirectsPanel',
+   ]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL': '',
+    'INTERCEPT_REDIRECTS': False,
+}
