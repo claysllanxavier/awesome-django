39f2ceef0e49c8c319fdfc49b3bbf0c3d0b01639
Claysllan Xavier <claysllan-xavier@hotmail.com>
2019-03-15 11:27:10 -0300

    :wrench: Muda os validores de senha
    - Coloca os validadores de senha somente no arquivo de produção

--- awesome_django/settings/production.py
+++ awesome_django/settings/production.py
@@ -11,6 +11,24 @@ CACHES = {
     }
 }
 
+# Password validation
+# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 EMAIL_BACKEND = env('EMAIL_BACKEND')
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = env('EMAIL_PORT')
