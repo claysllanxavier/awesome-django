575b697bbdc1636fc85cd9abdddde49f8121be4d
Claysllan Xavier <claysllan-xavier@hotmail.com>
2019-03-20 12:44:59 -0300

    :books: Coloca informações do projeto
    - Insere as informações do projeto no init

---
+++ awesome_django/__init__.py
@@ -0,0 +1,7 @@
+VERSION = (1, 0, 0)
+__version__ = '.'.join([str(i) for i in VERSION])
+__author__ = 'Claysllan Ferreira Xavier'
+__email__ = 'claysllan@gmail.com'
+__url__ = 'https://github.com/claysllanxavier/awesome-django'
+__license__ = 'MIT'
+__keywords__ = ['boilerplate', 'manager']
