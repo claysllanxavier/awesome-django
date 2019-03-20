b7548f0e94abd5fc0d3196b0e3c088856d439a12
Claysllan Xavier <claysllan-xavier@hotmail.com>
2019-03-20 20:27:08 -0300

    :sparkles: Começa o desenvolvimento de testes
    - Configura o settings de teste
    - Instala o coverage para inicar o testes da aplicação

--- awesome_django/settings/test.py
+++ awesome_django/settings/test.py
@@ -1,5 +1,8 @@
 from .base import *
 
+INSTALLED_APPS += [
+    'demo'
+]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -8,8 +11,11 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.sqlite3'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
+    }
 }
 
 PASSWORD_HASHERS = (
