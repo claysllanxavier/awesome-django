9f7c83b52fbc95f5946e2623d913aa1b95843f3f
Claysllan Xavier <claysllan-xavier@hotmail.com>
2019-03-22 10:48:08 -0300

    :bug: Corrige bug na páginação da api

--- awesome_django/settings/base.py
+++ awesome_django/settings/base.py
@@ -112,8 +112,7 @@ MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
-    'DEFAULT_PAGINATION_CLASS': (
-                        'rest_framework.pagination.PageNumberPagination',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': (
                         'django_filters.rest_framework.DjangoFilterBackend',)
